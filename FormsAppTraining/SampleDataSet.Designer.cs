b6ea630b55ca0a1357bf28652ca4060bf3f071af
Murali Krishnamurthy <muralimk@outlook.com>
2022-02-01 08:32:34 +0530

    Add project files.

---
+++ FormsAppTraining/SampleDataSet.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace FormsAppTraining
+{
+    partial class SampleDataSet
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvDataset = new System.Windows.Forms.DataGridView();
+            this.btnInsert = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnGet = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDataset)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvDataset
+            // 
+            this.dgvDataset.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDataset.Location = new System.Drawing.Point(249, 29);
+            this.dgvDataset.Name = "dgvDataset";
+            this.dgvDataset.Size = new System.Drawing.Size(497, 264);
+            this.dgvDataset.TabIndex = 0;
+            // 
+            // btnInsert
+            // 
+            this.btnInsert.Location = new System.Drawing.Point(310, 320);
+            this.btnInsert.Name = "btnInsert";
+            this.btnInsert.Size = new System.Drawing.Size(157, 48);
+            this.btnInsert.TabIndex = 1;
+            this.btnInsert.Text = "Insert";
+            this.btnInsert.UseVisualStyleBackColor = true;
+            this.btnInsert.Click += new System.EventHandler(this.btnInsert_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(520, 320);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(157, 48);
+            this.btnDelete.TabIndex = 2;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnGet
+            // 
+            this.btnGet.Location = new System.Drawing.Point(310, 388);
+            this.btnGet.Name = "btnGet";
+            this.btnGet.Size = new System.Drawing.Size(157, 48);
+            this.btnGet.TabIndex = 3;
+            this.btnGet.Text = "Get";
+            this.btnGet.UseVisualStyleBackColor = true;
+            this.btnGet.Click += new System.EventHandler(this.btnGet_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(520, 388);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(157, 48);
+            this.btnUpdate.TabIndex = 4;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // SampleDataSet
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnGet);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnInsert);
+            this.Controls.Add(this.dgvDataset);
+            this.Name = "SampleDataSet";
+            this.Text = "DataSet Example";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDataset)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvDataset;
+        private System.Windows.Forms.Button btnInsert;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnGet;
+        private System.Windows.Forms.Button btnUpdate;
+    }
+}
+
---
+++ FormsAppTraining/SampleDataSet.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormsAppTraining
+{
+    public partial class SampleDataSet : Form
+    {
+        public SampleDataSet()
+        {
+            InitializeComponent();
+        }
+
+        private void btnInsert_Click(object sender, EventArgs e)
+        {
+            string ConString = ConfigurationManager.AppSettings["ConnectionString"];
+            string Query = "SELECT * FROM tblProduct";
+
+            SqlDataAdapter adapter = new SqlDataAdapter(Query, ConString);
+            DataSet dset = new DataSet();
+
+            adapter.Fill(dset, "tblProduct");
+            dgvDataset.DataSource = dset.Tables["tblProduct"];
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            string ConString = ConfigurationManager.AppSettings["ConnectionString"];
+            string Query = "SELECT * FROM tblProduct";
+            SqlDataAdapter adapter = new SqlDataAdapter(Query, ConString);
+            DataSet set = new DataSet();
+            adapter.Fill(set, "tblProduct");
+
+            set.Tables["tblProduct"].Rows[1].Delete();
+
+            dgvDataset.DataSource = set.Tables["tblProduct"];
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            string ConString = ConfigurationManager.AppSettings["ConnectionString"];
+            string Query = "SELECT * FROM tblProduct";
+            SqlDataAdapter adapter = new SqlDataAdapter(Query, ConString);
+            DataSet set = new DataSet();
+            adapter.Fill(set, "tblProduct");           
+
+            dgvDataset.DataSource = set.Tables["tblProduct"];
+            
+        }
+
+        private void btnGet_Click(object sender, EventArgs e)
+        {
+            string ConString = ConfigurationManager.AppSettings["ConnectionString"];
+            string Query = "SELECT * FROM tblProduct";
+
+            SqlDataAdapter adapter = new SqlDataAdapter(Query, ConString);
+            DataSet set = new DataSet();
+
+            adapter.Fill(set, "tblProduct");
+            dgvDataset.DataSource = set.Tables["tblProduct"];
+        }
+    }
+}
